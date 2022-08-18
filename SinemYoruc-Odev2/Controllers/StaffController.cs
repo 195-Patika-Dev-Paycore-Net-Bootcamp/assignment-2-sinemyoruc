5cbe2465048152b79bf0853a4aa9e7f66f84cfd8
sinemyoruc <sinemyoruc@gmail.com>
2022-08-18 16:26:51 +0300

    HW2 added

---
+++ SinemYoruc-Odev2/Controllers/StaffController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SinemYoruc_Odev2.Controllers
+{
+
+    [Route("api/[controller]s")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        List<Staff> list = new(); //Staff turunde yeni nesne olusturuldu
+        public StaffController()
+        {
+            list.Add(new Staff { Id = 1, Name = "Deny", Lastname = "Sellen", DateOfBirth = new DateTime(1989, 1, 1), Email = "deny@gmail.com", PhoneNumber = "+90555443366", Salary = 4450 }); //Listeye eleman eklendi
+        }
+        private ActionResult<List<Staff>> GetList() //Listenin elemanlarini almasi icin genel bir method yazildi
+        {
+            return new ActionResult<List<Staff>>(list);
+        }
+
+
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult<List<Staff>> GetAll() //Listenin tum elemanlarini getiren Get methodu 
+        {
+            return GetList();
+        }
+
+
+        [HttpGet("GetById/{id}")]
+        public ActionResult<Staff> GetById([FromRoute] long id) //id'ye gore listenin istenen elemanini getiren GetById methodu
+        {
+            ActionResult<List<Staff>> list = GetList();  //listenin elemanlari alindi
+            Staff staff = list.Value.Where(x => x.Id == id).FirstOrDefault(); //staffin id'si alindi
+            return new ActionResult<Staff>(staff); //id'si alinan staff donduruldu
+        }
+
+
+        [HttpPost]
+        public ActionResult<List<Staff>> CreateStaff([FromBody] Staff staff) //Yeni staff ekleyen Post methodu
+        {
+            var list = GetList().Value; //listenin elemanlari alindi
+            list.Add(staff); //staff eklendi
+            return new ActionResult<List<Staff>>(list); //Yeni liste donduruldu
+        }
+
+        [HttpPut]
+        public ActionResult<List<Staff>> UpdateStaff(int id, [FromBody] Staff updateStaff) //Staff guncelleyen Put methodu
+        {
+            List<Staff> list = GetList().Value; //listenin elemanlari alindi
+            Staff staff = list.Where(x => x.Id == id).FirstOrDefault(); //Guncellenecek staffin idsi alindi
+            list.Remove(staff); //Mevcut staff silindi
+            updateStaff.Id = id; //girilen id ile yeni staffin idsi esitlendi
+            list.Add(updateStaff); //yeni staff listeye eklendi
+            return new ActionResult<List<Staff>>(list.ToList()); //Yeni liste donduruldu
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<List<Staff>> DeleteStaff([FromRoute] int id)
+        {
+            List<Staff> list = GetList().Value; //listenin elemanlari alindi
+            Staff staff = list.Where(x => x.Id == id).FirstOrDefault(); //silinecek staffin idsi alindi
+            list.Remove(staff); //silme islemi
+            return new ActionResult<List<Staff>>(list); //Yeni liste donduruldu
+        }
+    }
+}
---
+++ SinemYoruc-Odev2/Staff.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SinemYoruc_Odev2
+{
+    public class Staff
+    {
+        public long Id { get; set; }
+
+        [StringLength(maximumLength: 120, MinimumLength = 20, ErrorMessage = "Name must be range in 20-120")] //Name alani 20 ve 120 karakter arasinda olmali
+        [RegularExpression(@"^[a-zA-Z\s]*$")] //Rakamlara ve ozel karakterlere izin verilmiyor, bosluklara izin veriliyor
+        public string Name { get; set; }
+
+
+        [StringLength(maximumLength: 120, MinimumLength = 20, ErrorMessage = "Lastname must be range in 20-120")] //Lastname alani 20 ve 120 karakter arasinda olmali
+        [RegularExpression(@"^[a-zA-Z\s]*$")] //Rakamlara ve ozel karakterlere izin verilmiyor, bosluklara izin veriliyor
+        public string Lastname { get; set; }
+
+
+        //[RegularExpression(@"[0-9]{2}[\/\-\.][0-9]{2}[\/\-\.][0-9]{2,4}")]       //DateTime formatina uymadigi için hata aliyor, degisken string olursa calisir
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime), "11/11/1945", "10/10/2002")] //dogum tarihi 11/11/1945 ve 10/10/2002 arasinda olmali
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+
+         [Required]
+         [EmailAddress(ErrorMessage = "Email address is not valid.")] //@ ve . kontrolu yapiliyor
+        [RegularExpression(@"^[a-zA-Z]*$")]
+        public string Email { get; set; }
+
+
+
+         [Required]
+         [RegularExpression(@"^[+]905[0-9]{9}$")]  //Telefon numarasının +905 ile baslayıp 9 rakam daha ekleme validasyonu
+         [Phone(ErrorMessage = "Phone is not valid.")] 
+         public string PhoneNumber { get; set; }
+
+
+
+
+         [Range(minimum: 2000, maximum: 9000, ErrorMessage = "Salary must be range in 2000-9000")] //Maas 2000 ve 9000 arasinda olmali
+         public double Salary { get; set; }
+    }
+}
