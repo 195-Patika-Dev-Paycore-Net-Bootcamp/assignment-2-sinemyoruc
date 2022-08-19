1ccaebb9b2420c83ce8de2f0fa8d05e9d39214dc
sinemyoruc <sinemyoruc@gmail.com>
2022-08-19 16:55:03 +0300

    comment fix

--- SinemYoruc-Odev2/Staff.cs
+++ SinemYoruc-Odev2/Staff.cs
@@ -10,38 +10,46 @@ namespace SinemYoruc_Odev2
     {
         public long Id { get; set; }
 
-        [StringLength(maximumLength: 120, MinimumLength = 20, ErrorMessage = "Name must be range in 20-120")] //Name alani 20 ve 120 karakter arasinda olmali
-        [RegularExpression(@"^[a-zA-Z\s]*$")] //Rakamlara ve ozel karakterlere izin verilmiyor, bosluklara izin veriliyor
+        //Name alani 20 ve 120 karakter arasinda olmali
+        [StringLength(maximumLength: 120, MinimumLength = 20, ErrorMessage = "Name must be range in 20-120")]
+        //Rakamlara ve ozel karakterlere izin verilmiyor, bosluklara izin veriliyor
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = ("Please do not enter numbers and special characters"))]
         public string Name { get; set; }
 
 
-        [StringLength(maximumLength: 120, MinimumLength = 20, ErrorMessage = "Lastname must be range in 20-120")] //Lastname alani 20 ve 120 karakter arasinda olmali
-        [RegularExpression(@"^[a-zA-Z\s]*$")] //Rakamlara ve ozel karakterlere izin verilmiyor, bosluklara izin veriliyor
+        //Lastname alani 20 ve 120 karakter arasinda olmali
+        [StringLength(maximumLength: 120, MinimumLength = 20, ErrorMessage = "Lastname must be range in 20-120")]
+        //Rakamlara ve ozel karakterlere izin verilmiyor, bosluklara izin veriliyor
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = ("Please do not enter numbers and special characters"))]
         public string Lastname { get; set; }
 
 
-        [RegularExpression(@"[0-9]{2}[\-\/\.][0-9]{2}[\/\-\.][0-9]{2,4}")]  //01-01-1989 formati icin yapildi
-        [Range(typeof(DateTime), "11/11/1945", "10/10/2002")] //dogum tarihi 11/11/1945 ve 10/10/2002 arasinda olmali
+        //01-01-1989 formati icin yapildi
+        [RegularExpression(@"[0-9]{2}[\-\/\.][0-9]{2}[\/\-\.][0-9]{2,4}")]
+        //dogum tarihi 11/11/1945 ve 10/10/2002 arasinda olmali
+        [Range(typeof(DateTime), "11/11/1945", "10/10/2002")]
         [Required]
-         public string DateOfBirth { get; set; }
+        public string DateOfBirth { get; set; }
 
 
-         [Required]
-         [EmailAddress]
-         [RegularExpression(@"^[a-zA-Z]+\@+[a-zA-Z]+\.+[a-zA-Z]*$")] //@, nokta ve harf haric karakter girilmemesini sagliyor
-         public string Email { get; set; }
-
-
-
-         [Required]
-         [RegularExpression(@"^[+]905[0-9]{9}$")]  //Telefon numarasının +905 ile baslayıp 9 rakam daha ekleme validasyonu
-         [Phone(ErrorMessage = "Phone is not valid.")] 
-         public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        //@, nokta ve harf haric karakter girilmemesini sagliyor
+        [RegularExpression(@"^[a-zA-Z]+\@+[a-zA-Z]+\.+[a-zA-Z]*$", ErrorMessage = ("Please enter valid email"))]
+        public string Email { get; set; }
 
 
 
+        [Required]
+        //Telefon numarasının +905 ile baslayıp 9 rakam daha ekleme validasyonu
+        [RegularExpression(@"^[+]905[0-9]{9}$", ErrorMessage = ("Please enter valid phone number"))]
+        [Phone(ErrorMessage = "Phone is not valid.")]
+        public string PhoneNumber { get; set; }
 
-         [Range(minimum: 2000, maximum: 9000, ErrorMessage = "Salary must be range in 2000-9000")] //Maas 2000 ve 9000 arasinda olmali
-         public double Salary { get; set; }
+
+
+        //Maas 2000 ve 9000 arasinda olmali
+        [Range(minimum: 2000, maximum: 9000, ErrorMessage = "Salary must be range in 2000-9000")]
+        public double Salary { get; set; }
     }
 }
